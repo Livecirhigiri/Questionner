988a78209ecb0ac8e227f09e93c79dc9d5c3e8a7
live Cirhigiri <cimancirh@gmail.com>
2019-01-18 01:25:10 +0200

    test rsvp

--- server/tests/testRsvp.js
+++ server/tests/testRsvp.js
@@ -14,8 +14,23 @@ describe('User', () => {
                 .post('/api/v1/rsvps')
                 .send(data)
                 .end((err, res) => {
-                    expect(res.status).to.eql(200);
+                    expect(res.status).to.deep.equal(400);
+                    done();
                 });
         });
+    });
+});
+
+// eslint-disable-next-line no-undef
+describe('send rsvp ', () => {
+    it('send All rsvp ', (done) => {
+        chai
+            .request(server)
+            .get('/api/v1/rsvps')
+            .set('Accept', 'application/json')
+            .end((err, res) => {
+                expect(res.status).to.deep.equal(200);
+                done();
+            });
     });
 });
