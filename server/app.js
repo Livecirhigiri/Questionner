cdd9042861fe9b883458ecfd533a3031fa3053fd
live Cirhigiri <cimancirh@gmail.com>
2019-01-18 12:19:23 +0200

    Ignore node modules

--- server/app.js
+++ server/app.js
@@ -1,16 +1,16 @@
-const express = require("express");
-const morgan = require("morgan");
-const bodyParser = require("body-parser");
-const userRoutes = require("./routes/userRoutes");
-const meetupRoutes = require("./routes/meetupRoutes");
-const questionRoutes = require("./routes/questionRoutes");
-const rsvpRoutes = require("./routes/rsvpRoutes");
+const express = require('express');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const userRoutes = require('./routes/userRoutes');
+const meetupRoutes = require('./routes/meetupRoutes');
+const questionRoutes = require('./routes/questionRoutes');
+const rsvpRoutes = require('./routes/rsvpRoutes');
 
-const port = 5000;
+const port = parseInt(process.env.PORT, 10) || 5000;
 const app = express();
 
 // routes  to handle requests
-app.use(morgan("dev"));
+app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(userRoutes);
@@ -34,7 +34,7 @@ app.use((error, req, res, next) => {
 */
 // port
 app.listen(port, () => {
-  console.log("app is listening on port", port);
+    console.log('app is listening on port', port);
 });
 
 module.exports = app;
