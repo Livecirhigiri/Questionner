cd7199b0eccacfa68bb91257c76b6f2794d2eee3
live Cirhigiri <cimancirh@gmail.com>
2019-01-22 22:01:51 +0200

    get a specific user

--- server/controllers/meetup.js
+++ server/controllers/meetup.js
@@ -12,4 +12,47 @@ module.exports = {
             });
         });
     },
+
+    meetupId: (req, res) => {
+        const id_meetup = parseInt(req.params.id, 10);
+        pool.query(
+            'SELECT * FROM meetups WHERE id_meetup = $1',
+            [id_meetup],
+            (err, result) => {
+                if (err) {
+                    throw err;
+                }
+                if (result.rows.length === 0) {
+                    return res.status(404).json({
+                        status: 404,
+                        error: 'user not found',
+                    });
+                }
+                res.status(200).json({
+                    status: 200,
+                    data: result.rows,
+                });
+            },
+        );
+    },
+
+    registerMeetup: (req, res) => {
+        const {
+ createdon, image, topic, hapeningon, tags 
+} = req.body;
+        pool.query(
+            'INSERT INTO meetups (createdon,image, topic, hapeningon, tags) VALUES ($1,$2,$3,$4,$5) returning *',
+            [createdon, image, topic, hapeningon, tags],
+            (err, res) => {
+                if (err) {
+                    throw err;
+                } else {
+                    res.status(201).json({
+                        status: 201,
+                        data: [req.body],
+                    });
+                }
+            },
+        );
+    },
 };
--- server/routes/meetup.js
+++ server/routes/meetup.js
@@ -3,12 +3,9 @@ const express = require('express');
 const app = express.Router();
 const meetupcontrollers = require('../controllers/meetup');
 
-// app.get("/", meetupcontrollers.getMeetup);
-// app.post("/", meetupcontrollers.registerMeetup);
-
-// app.post('/api/v1/meetups', userController.registerM);
+app.post('/', meetupcontrollers.registerMeetup);
 app.get('/', meetupcontrollers.getMeetup);
-// app.get('/api/v1/meetups/:id', userController.meetupId);
+app.get('/:id', meetupcontrollers.meetupId);
 // app.patch('/api/v1/meetups/:id', userController.updateMeetup);
 // app.delete('/api/v1/meetups/:id', userController.deleteMeetup);
 
