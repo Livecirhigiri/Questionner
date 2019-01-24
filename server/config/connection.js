87458c29c0c8d3d9d90740abd21af93e954d3327
live Cirhigiri <cimancirh@gmail.com>
2019-01-24 17:59:33 +0200

    validate update meetUp

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,11 +1,22 @@
 const { Pool } = require('pg');
+const dotenv = require('dotenv');
 
-const connectionString = new Pool({
-    user: 'Superuser',
-    host: 'localhost',
-    database: 'endpointDB',
-    password: 'olivialive',
-    port: 5432,
-});
+dotenv.config();
 
-module.exports = connectionString;
+let conString;
+if (process.env.DATABASE_URL) {
+    conString = new Pool({
+        connectionString: process.env.DATABASE_URL,
+    });
+} else {
+    conString = new Pool({
+        user: 'Superuser',
+        host: 'localhost',
+        database: 'endpointDB',
+        password: 'olivialive',
+        port: 5432,
+    });
+}
+
+
+module.exports = conString;
